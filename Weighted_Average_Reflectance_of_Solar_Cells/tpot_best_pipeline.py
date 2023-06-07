ef70ec4782dddd49d943379042699daa23f90636
umkiyoung <se99an@gmail.com>
2023-06-08 00:39:38 +0900

    Update IMG_to_Thickness

--- Weighted_Average_Reflectance_of_Solar_Cells/tpot_best_pipeline.py
+++ Weighted_Average_Reflectance_of_Solar_Cells/tpot_best_pipeline.py
@@ -7,10 +7,10 @@ from sklearn.pipeline import make_pipeline
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
-tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
-features = tpot_data.drop('target', axis=1)
-training_features, testing_features, training_target, testing_target = \
-            train_test_split(features, tpot_data['target'], random_state=42)
+tpot_data = pd.read_csv('tpot_best_pipeline.csv').drop(columns=['Unnamed: 0'])
+train, test = train_test_split(tpot_data, test_size=0.3, random_state=42)
+training_features, training_target = train.iloc[:,:4], train.iloc[:,4]
+testing_features, testing_target = test.iloc[:,:4], test.iloc[:,4]
 
 # Average CV score on the training set was: -8.673579471970266
 exported_pipeline = make_pipeline(

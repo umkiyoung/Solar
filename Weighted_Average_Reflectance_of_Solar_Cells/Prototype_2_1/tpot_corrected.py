349b7b2891cf4e611ebbe7f6e3f8630ceab00ff0
umkiyoung <se99an@gmail.com>
2023-07-20 19:17:58 +0900

    Update 7th_other_calibration.ipynb Update 7th_other_calibration_2.ipynb Update 7th_other_calibration_3.ipynb Update tpot_corrected.py Update README.md Update README.md Update README.md Update README.md

--- Weighted_Average_Reflectance_of_Solar_Cells/Prototype_2_1/tpot_corrected.py
+++ Weighted_Average_Reflectance_of_Solar_Cells/Prototype_2_1/tpot_corrected.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.feature_selection import SelectFwe, f_regression
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from tpot.builtins import StackingEstimator
-from xgboost import XGBRegressor
+from sklearn.preprocessing import Binarizer, RobustScaler
+from tpot.builtins import StackingEstimator, ZeroCount
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -14,11 +14,13 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: -48.66765317278573
+# Average CV score on the training set was: -246.62237831453194
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=XGBRegressor(learning_rate=0.01, max_depth=7, min_child_weight=20, n_estimators=100, n_jobs=1, objective="reg:squarederror", subsample=0.6000000000000001, verbosity=0)),
-    SelectFwe(score_func=f_regression, alpha=0.032),
-    AdaBoostRegressor(learning_rate=0.5, loss="square", n_estimators=100)
+    RobustScaler(),
+    Binarizer(threshold=0.4),
+    ZeroCount(),
+    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.9, learning_rate=0.01, loss="quantile", max_depth=10, max_features=0.7000000000000001, min_samples_leaf=9, min_samples_split=12, n_estimators=100, subsample=0.9000000000000001)),
+    ElasticNetCV(l1_ratio=0.5, tol=1e-05)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
